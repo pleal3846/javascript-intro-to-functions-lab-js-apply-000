8829be55c31bc6338869fd1636ad6874489ef8e5
patrick leal <pjleal3846@icloud.com>
2019-12-03 20:33:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can't hear you!"
+  } else if (string === string.toUpperCase()){
+    return 
+    
   }
   
 }
